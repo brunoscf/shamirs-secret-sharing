eb7f11e95d67cc99bc4dc0269fa47c54e95e7bcb
BrunoSCF <brunoscf5@outlook.com>
2020-04-28 21:05:34 -0400

    generate OK and get_secret OK

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from shamir_secret_sharing import ShamirSecretSharing
 
-SECRET_CODE = 255
-QUANT_PUBLIC_KEYS = 5
+SECRET_CODE = 1267
+QUANT_PUBLIC_KEYS = 9
 
 sss = ShamirSecretSharing()
-keys = sss.generate_public_keys(SECRET_CODE, QUANT_PUBLIC_KEYS)
-print(keys)
+# keys = sss.generate_public_keys(SECRET_CODE, QUANT_PUBLIC_KEYS)
+# print(keys)
+
+secret_code = sss.get_secret_code('75766068', '37828', '943047988', '816778483', '11268', '21523', '61680883', '466803', '5391892')
+print(secret_code)
--- shamir_secret_sharing.py
+++ shamir_secret_sharing.py
@@ -4,11 +4,20 @@ from scipy.interpolate import lagrange
 
 class ShamirSecretSharing:
     def _function(self, x, root, degree):
-        return x **degree + root
+        return x**degree + root
 
     def generate_public_keys(self, secret_code, quant_public_keys):
-        X = np.random.choice(np.arange(1, 9), size=quant_public_keys-1, replace=False)
+        X = np.random.choice(np.arange(1, 10), size=quant_public_keys, replace=False)
         Y = [self._function(x=xi, root=secret_code, degree=quant_public_keys-1) for xi in X]
         
         public_keys = [str(x)+str(y) for x,y in zip(X, Y)]
         return public_keys
+
+    def get_secret_code(self, *keys):
+        X,Y = [],[]
+        for key in keys:
+            X.append(int(key[:1]))
+            Y.append(int(key[1:]))
+
+        poly_function = lagrange(X, Y)
+        return int(poly_function(0))
